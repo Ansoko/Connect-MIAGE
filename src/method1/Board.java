c7588c7ba96aa0996dadcd7ab37f95bb3c3b0167
Ansoko <ansoko@git.com>
2020-02-01 16:55:38 +0100

    recherche itérative

--- src/method1/Board.java
+++ src/method1/Board.java
@@ -43,7 +43,7 @@ public class Board {
 	}
 
 	public void iterativResearch() {
-		
+
 		//on ne s'occupe pas du marquage des bords dans un premier temps
 		for(int i=1; i<board.length-1; i++) {
 			for(int j=1; j<board[i].length-1; j++) {
@@ -54,12 +54,12 @@ public class Board {
 					board[i][j].setState(marquage.DeadEnd);
 				}
 			}
-			
+
 			//marquage colonne collée à gauche
 			if(!board[i][0].isConnected(board[i-1][0], 1) || !board[i][0].isConnected(board[i][1], 2) || !board[i][0].isConnected(board[i+1][0], 3) || !board[i][0].isConnected(null, 4)) { 
 				board[i][0].setState(marquage.DeadEnd);
 			}
-			
+
 			//colonne collée à droite
 			if(!board[i][board[i].length-1].isConnected(board[i-1][board[i].length-1], 1) || !board[i][board[i].length-1].isConnected(null, 2) || !board[i][board[i].length-1].isConnected(board[i+1][board[i].length-1], 3) || !board[i][board[i].length-1].isConnected(board[i][board[i].length-2], 4)) { 
 				board[i][board[i].length-1].setState(marquage.DeadEnd);
@@ -98,13 +98,28 @@ public class Board {
 			board[LINE-1][ROW-1].setState(marquage.DeadEnd);
 		}
 
-		
-		//affichage des marquages
-		for(int i=0; i<board.length; i++) {
-			for(int j=0; j<board[i].length; j++) {
-				System.out.print(board[i][j].getState() + " ");
+
+		//on reparcourt le tableau en s'arrêtant que sur les tuilles sans marquage
+		boolean mark = true;
+		while(mark) {
+			mark = false;
+			for(int i=0; i<board.length; i++) {
+				for(int j=0; j<board[i].length; j++) {
+					if(board[i][j].getState()==null) {
+						//on regarde ses voisins de branche, si ils sont marqué alors on marque aussi
+							//en haut
+						if(board[i][j].getNum()==1 || board[i][j].getNum()==3 || board[i][j].getNum()==6 || board[i][j].getNum()==7) {
+							if(board[i-1][j].getState()==marquage.DeadEnd) {
+								board[i][j].setState(marquage.DeadEnd);
+								mark = true;
+							}
+						}
+							//à droite
+							//en bas
+							//à gauche
+					}
+				}
 			}
-			System.out.println("\n");
 		}
 	}
 
