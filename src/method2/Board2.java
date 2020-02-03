6d2ef4099f8922b0f45447535f916802d806b25b
Valentine BOUCHE <valentine.bouche.stjo@gmail.com>
2020-02-03 13:21:30 +0100

    Bug Fixed

--- src/method2/Board2.java
+++ src/method2/Board2.java
@@ -2,13 +2,13 @@ package method2;
 import java.util.Arrays;
 import javax.swing.JFrame;
 
-public class Board {
+public class Board2 {
 	public final int LINE = 3;
 	public final int ROW = 5 ;
 	private Circuit longest;
 	private LiteTile board[][];
 
-	public Board() { // automatiques avec les constantes
+	public Board2() { // automatiques avec les constantes
 		board = new LiteTile[LINE][ROW];
 		longest = new Circuit();
 	}
@@ -16,9 +16,8 @@ public class Board {
 	public void cornerResearch() { // recherche des coins se 
 		for(int i = 0; i < LINE; i++ ) {
 			for (int j = 0; j < ROW; j++) {
-				if(this.board[i][j] != null && this.isCornerSE()) { //on trouve un coin se
+				if(board[i][j] == null && board[i][j].isCornerSE() && !board[i][j].isMark()) //on trouve un coin se
 					followCircuit(i,j); // on parcourt le circuit
-				}
 			}
 		}
 	}
@@ -30,16 +29,16 @@ public class Board {
 		boolean start = false; // start devient vrai si le circuit est arrivé au début
 	
 		while(flag && !start) {
-			if(this.board[posI][posJ].getE() && startJ+1 < LINE) {//si la case à droite n'est pas un mur
+			if(board[posI][posJ].isE() && startJ+1 < LINE) {//si la case à droite n'est pas un mur
 				posJ +=1; //on va vers la droite
 			}
-			else if(this.board[posI][posJ].getN() && startI-1 >= 0) {//si la case au nord n'est pas un mur
+			else if(this.board[posI][posJ].isN() && startI-1 >= 0) {//si la case au nord n'est pas un mur
 				posI -=1; //on va vers le haut
 			}
-			else if(this.board[posI][posJ].getW() && startJ-1 >= 0) {//si la case à l'ouest n'est pas un mur
+			else if(this.board[posI][posJ].isW() && startJ-1 >= 0) {//si la case à l'ouest n'est pas un mur
 				posJ -=1; //on va vers le haut
 			}
-			else if(this.board[posI][posJ].getS() && startI+1 > ROW) {//si la case au sud n'est pas un mur
+			else if(this.board[posI][posJ].isS() && startI+1 > ROW) {//si la case au sud n'est pas un mur
 				posI +=1; //on va vers le bas
 			}
 			else {
@@ -49,7 +48,7 @@ public class Board {
 			if(posI==startI && posJ == startJ)
 				start = true;
 			else {
-				if(this.board[posI][posJ].getNum() == 0) {
+				if(this.board[posI][posJ] == null) {
 					flag = false;
 				}else {
 					//si la case d'avant est marquée ou est le start alors on vient de là
