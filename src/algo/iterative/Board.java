49982d0ca47b7356377fe43a551857ff41ce8962
Ansoko <45961184+Ansoko@users.noreply.github.com>
2020-04-13 17:28:40 +0200

    Optimisation lignes algo ité

--- src/algo/iterative/Board.java
+++ src/algo/iterative/Board.java
@@ -59,69 +59,39 @@ public class Board {
 	}
 
 	public void iterativResearch() {
-		//on ne s'occupe pas de marquer les bords dans un premier temps
-		for(int i=1; i<board.length-1; i++) {
-			for(int j=1; j<board[i].length-1; j++) {
+		for(int i=0; i<board.length; i++) {
+			for(int j=0; j<board[i].length; j++) {
 				if(isEmpty(i, j)) {
 					board[i][j].setState(mark.DeadEnd);
-				}else if(!board[i][j].isConnected(board[i-1][j], 1) || !board[i][j].isConnected(board[i][j+1], 2) || !board[i][j].isConnected(board[i+1][j], 3)  || !board[i][j].isConnected(board[i][j-1], 4)) { 
-					//on regarde à chaque fois si elle est non connectée à tous ses bords
-					board[i][j].setState(mark.DeadEnd);
+				}else{  //on regarde à chaque fois si elle est non connectée à tous ses bords	
+					//en haut
+					if(board[i][j].getExit("up") && (i==0 || !board[i-1][j].getExit("down")) ){
+						board[i][j].setState(mark.DeadEnd);
+					}
+					//à droite
+					else if(board[i][j].getExit("right") && (j==board[i].length-1 || !board[i][j+1].getExit("left")) ) {
+						board[i][j].setState(mark.DeadEnd);
+					}
+					//en bas
+					else if(board[i][j].getExit("down") && (i==board.length-1 || !board[i+1][j].getExit("up")) ) {
+						board[i][j].setState(mark.DeadEnd);
+					}
+					//à gauche
+					else if(board[i][j].getExit("left") && (j==0 || !board[i][j-1].getExit("right")) ) {
+						board[i][j].setState(mark.DeadEnd);
+					}
 				}
 			}
-
-			//mark colonne collée à gauche
-			if(!board[i][0].isConnected(board[i-1][0], 1) || !board[i][0].isConnected(board[i][1], 2) || !board[i][0].isConnected(board[i+1][0], 3) || !board[i][0].isConnected(null, 4)) { 
-				board[i][0].setState(mark.DeadEnd);
-			}
-
-			//colonne collée à droite
-			if(!board[i][board[i].length-1].isConnected(board[i-1][board[i].length-1], 1) || !board[i][board[i].length-1].isConnected(null, 2) || !board[i][board[i].length-1].isConnected(board[i+1][board[i].length-1], 3) || !board[i][board[i].length-1].isConnected(board[i][board[i].length-2], 4)) { 
-				board[i][board[i].length-1].setState(mark.DeadEnd);
-			}
 		}
 
-		//première ligne
-		for(int j=1; j<board[LINE-1].length-1; j++) {
-			if(isEmpty(0, j)) {
-				board[0][j].setState(mark.DeadEnd);
-			}else if(!board[0][j].isConnected(null, 1) || !board[0][j].isConnected(board[0][j+1], 2) || !board[0][j].isConnected(board[1][j], 3) || !board[0][j].isConnected(board[0][j-1], 4)) { 
-				board[0][j].setState(mark.DeadEnd);
-			}
-		}
-
-		//dernière ligne
-		for(int j=1; j<board[LINE-1].length-1; j++) {
-			if(isEmpty(LINE-1, j)) {
-				board[LINE-1][j].setState(mark.DeadEnd);
-			}else if(!board[LINE-1][j].isConnected(board[LINE-2][j], 1) || !board[LINE-1][j].isConnected(board[LINE-1][j+1], 2) || !board[LINE-1][j].isConnected(null, 3) || !board[LINE-1][j].isConnected(board[LINE-1][j-1], 4)) { 
-				board[LINE-1][j].setState(mark.DeadEnd);
-			}
-		}
-
-		//coins
-		if(board[0][0].getNum()!=4 || (!board[0][0].isConnected(board[0][1], 2) || !board[0][0].isConnected(board[1][0], 3))) { 
-			board[0][0].setState(mark.DeadEnd);
-		}
-		if(board[0][ROW-1].getNum()!=5 || (!board[0][ROW-1].isConnected(board[0][ROW-2], 4) || !board[0][ROW-1].isConnected(board[1][ROW-1], 3))) { 
-			board[0][ROW-1].setState(mark.DeadEnd);
-		}
-		if(board[LINE-1][0].getNum()!=3 || (!board[LINE-1][0].isConnected(board[LINE-2][0], 1) || !board[LINE-1][0].isConnected(board[LINE-1][1], 2))) { 
-			board[LINE-1][0].setState(mark.DeadEnd);
-		}
-		if(board[LINE-1][ROW-1].getNum()!=6 || (!board[LINE-1][ROW-1].isConnected(board[LINE-2][ROW-1], 1) || !board[LINE-1][ROW-1].isConnected(board[LINE-1][ROW-2], 4))) { 
-			board[LINE-1][ROW-1].setState(mark.DeadEnd);
-		}
-
-
-		//on reparcourt le tableau en s'arrêtant uniquement sur les tuilles sans mark
+		//on reparcourt le tableau en s'arrêtant uniquement sur les tuilles sans "mark"
 		boolean findMark = true;
 		while(findMark) {
 			findMark = false;
 			for(int i=0; i<board.length; i++) {
 				for(int j=0; j<board[i].length; j++) {
 					if(board[i][j].getState()==null) {
-						//on regarde ses voisins de branche, si ils sont marqu��� alors on marque aussi
+						//on regarde ses voisins de branche, si ils sont marqués alors on marque aussi
 						//en haut
 						if(board[i][j].getExit("up")) {
 							if(board[i-1][j].getState()==mark.DeadEnd) {
