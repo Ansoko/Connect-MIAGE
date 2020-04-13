abb92a9dc4d4db9dc2b26e704cfc8e6edb500c0d
Valentine Bouché <36455378+MoissBatt@users.noreply.github.com>
2020-04-13 19:05:51 +0200

    modifs

--- src/graphic/Graphic.java
+++ src/graphic/Graphic.java
@@ -24,14 +24,14 @@ public class Graphic extends JFrame  {
 		frame.setVisible(true);
 		int ref;
 		try {
-		    tabNb[0] = ImageIO.read(new File("Images/empty.png"));
-		    tabNb[1] = ImageIO.read(new File("Images/vertical.png"));
-		    tabNb[2] = ImageIO.read(new File("Images/horizontal.png"));
-		    tabNb[3] = ImageIO.read(new File("Images/downright.png"));
-		    tabNb[4] = ImageIO.read(new File("Images/downleft.png"));
-		    tabNb[5] = ImageIO.read(new File("Images/topright.png"));
-		    tabNb[6] = ImageIO.read(new File("Images/topleft.png"));
-		    tabNb[7] = ImageIO.read(new File("Images/cross.png"));
+		    tabNb[0] = ImageIO.read(new File("./Images/empty.png"));
+		    tabNb[1] = ImageIO.read(new File("./Images/vertical.png"));
+		    tabNb[2] = ImageIO.read(new File("./Images/horizontal.png"));
+		    tabNb[3] = ImageIO.read(new File("./Images/downright.png"));
+		    tabNb[4] = ImageIO.read(new File("./Images/downleft.png"));
+		    tabNb[5] = ImageIO.read(new File("./Images/topright.png"));
+		    tabNb[6] = ImageIO.read(new File("./Images/topleft.png"));
+		    tabNb[7] = ImageIO.read(new File("./Images/cross.png"));
 		    
 		    for(String[] line : tableTest) {
 		        for(String s : line) {
